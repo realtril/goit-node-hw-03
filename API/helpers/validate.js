4f44e60273c89d6b8adc4c53ddbf0cb61e2ac050
Oleksandr Tril <tril.oleksandr@gmail.com>
2020-09-22 21:17:43 +0300

    some fixes

--- API/helpers/validate.js
+++ API/helpers/validate.js
@@ -1,6 +1,6 @@
-exports.validate = function validate(scheme) {
+exports.validate = function validate(scheme, reqPart = "body") {
   return (req, res, next) => {
-    const validationResult = scheme.validate(req.body);
+    const validationResult = scheme.validate(req[reqPart]);
     if (validationResult.error) {
       return res.status(400).send(validationResult.error);
     }
